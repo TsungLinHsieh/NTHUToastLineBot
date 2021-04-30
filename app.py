7a4fa06528a9211c32e4ae363849b9cab9ef325a
Walter <walter0207.mse04g@g2.nctu.edu.tw>
2021-04-30 21:50:42 +0800

    recover

--- app.py
+++ app.py
@@ -37,17 +37,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	msg = event.message.text
-	r = '很抱歉 妳說甚麼'
+	# msg = event.message.text
+	# r = '很抱歉 妳說甚麼'
 
-	if msg == 'hi':
-		r = 'hi'
+	# if msg == 'hi':
+	# 	r = 'hi'
 
-	elif msg == '妳吃飯了嗎?':
-		r = '還沒'
+	# elif msg == '妳吃飯了嗎?':
+	# 	r = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
