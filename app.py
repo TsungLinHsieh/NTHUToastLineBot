13f323d0e2ed84863c5d8aaa53306c7cd0f7e74f
Walter <walter0207.mse04g@g2.nctu.edu.tw>
2021-04-30 22:20:25 +0800

    update2

--- app.py
+++ app.py
@@ -38,16 +38,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	msg=event.message.text
-	r = '''Hi there, thanks for adding me as friend! 
-	       Which information you want to know? 
-	       location, meeting time, contact window
-	       '''
-	if 'TME' in msg:
-		r = 'TME is Toastmaster of the evening.'
-	elif 'location'in msg:
-		r = 'NTHU Delta Hall R601 (清華大學 台達館 601室)'
-	elif 'time' in msg:
-		r = 'Meeting will be held from 19:00 to 21:00 every Thursday.'
+	r = "Hi there, thanks for adding me as friend! Which information you want to know? location, meeting time or contact window"
+
+	if 'location'or 'Location' in msg:
+		r = 'Meeting will take place at NTHU Delta Hall R601 (清華大學 台達館 601室)'
+	elif 'Time'or 'time' in msg:
+		r = 'Meeting will be held from 19:00 to 21:00 every Thursday'
+	elif 'contact window' or 'window' or 'Contact window' in msg:
+		r = 'You can contact Johnny for more information. LINE ID:'
 
 	line_bot_api.reply_message(
 		event.reply_token,
