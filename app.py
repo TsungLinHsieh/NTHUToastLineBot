029a31aa014b85dd5eb2e0a8faf61c1da857e055
Walter <walter0207.mse04g@g2.nctu.edu.tw>
2021-05-08 23:39:25 +0800

    chat

--- app.py
+++ app.py
@@ -42,24 +42,24 @@ def handle_message(event):
 	msg=event.message.text
 	r = '''Hi there, thanks for adding me as friend! I am a line-bot.\nWhich information you want to know?\n1 Location\n2 Meeting time\n3  contact window\n4 FB Fanpage\n5 About me\n6 Roles'''
 
-	chat = {'(1)':'I am a line-bot serving for NTHU toastmasters. And you are?',
-	'(2)':'I was first created on 2021-04-30 by Walter Hsieh. You do the math lol',
-	'(3)':'I like to chat with people even though we have nothing to say :)'
+	chat = {
+	'(1)':"No, you don't need to be fluent. Here we provide friendly and English immersing evironment for you to practice."
+	'(2)':"Don't worry about that. Only thing you need is to come and have fun."
+	'(3)':'Join our regular meeting, we will help to go through the process.'
+	'(4)':"Toastmasters is nonprofit educational organization which helps its members improve their public speaking skills and build their leadership style."
+	'(5)':'I am a line-bot serving for NTHU toastmasters. And you are?',
+	'(6)':'I was first created on 2021-04-30 by Walter Hsieh. You do the math lol',
+	'(7)':'I like to chat with people even though we have nothing to say :)'
 	}
 
 
-	Location = ['Location', 'location', '1', 'Location & Meeting Time']
+	Location = ['Location', 'location', '1']
 	Time = ['Time', 'time', 'meeting time', 'Meeting time','2']
 	Contact = ['Window','window','Contact','contact', 'Contact window','contact window','3']
 	FB = ['facebook', 'Facebook', 'fb', 'FB','Fanpage', 'fanpage', 'FB fanpage','FB Fanpage','4']
 	Me = ['About me','about me', 'me','Me','5']
 	roles = ['roles','role','Role', 'Roles','TME','Timer','counter','Counter','grammarian','Grammarian','Evaluator','evaluator', '6']
-	Q = [
-	'1. Should I speak English fluently?', 
-	'2. Should I learn any thing before I join?', 
-	'3. How to join this club?',
-	'4. What is Toastmaster?'
-	]
+
 
 
 	if msg in Location:
@@ -72,7 +72,7 @@ def handle_message(event):
 		r = 'Meeting will be held from 19:00 to 21:00 every Thursday'
 
 	elif msg in Contact:
-		r = 'You can contact Johnny (President, LINE ID:),\n Walter (LINE ID: Walter0309) for more information,\nor Go to our FB fan page and leave us message by Messenger.'
+		r = 'You can contact Johnny (President, LINE ID:),\nWalter (LINE ID: Walter0309) for more information,\nOr go to our FB fan page and leave us message by Messenger.'
 
 	elif msg in FB:
 		r = 'https://www.facebook.com/nthutoastmasters/'
@@ -81,14 +81,14 @@ def handle_message(event):
 		r = chat[msg]
 
 	elif msg in Me:
-		r = 'Questions about me are listed below\n(1)Who are you?\n(2)How old are you?\n(3)What is your hobby?'
+		r = 'Questions about me are listed below\n(5)Who are you?\n(6)How old are you?\n(7)What is your hobby?'
 	
 	elif msg in roles:
 		r = 'https://www.toastmasters.org/membership/club-meeting-roles'
 
 	elif msg == 'Q&A':
-		for i in Q:
-			r = Q[i]
+		r = '(1) Should I speak English fluently?\n(2) Should I learn any thing before I join?\n(3) How to join this club?\n(4) What is Toastmaster?'
+
 
 	line_bot_api.reply_message(
 		event.reply_token,
